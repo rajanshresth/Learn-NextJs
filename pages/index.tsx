a189fdf259909a492adc764091f3bc96e42c6b4c
Rajan <shrestharajan726@gmail.com>
2023-01-14 18:17:13 +0545

    1st

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-6xl font-bold">
+          Rajan
+        </h1>
+      </main>
+        
+    </div>
+  )
+}
+
+export default Home
